41ee0f2fb6283d5f0f7fcf3df382dff5eed330a3
Priodyuti Pradhan <priodyuti@users.noreply.github.com>
2022-08-14 18:33:44 +0530

    Delete Theta.d

--- Theta.d
+++
@@ -1,4 +0,0 @@
-/home/gautam/Dropbox/DCTSystem/obj/Theta.o: \
- /home/gautam/Dropbox/DCTSystem/src/Theta.cpp \
- /home/gautam/Dropbox/DCTSystem/src/Theta.h \
- /home/gautam/Dropbox/DCTSystem/src/ContactVector.h
