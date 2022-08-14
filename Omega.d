eff40746d80df70834d374f5da03d3d9195ba12f
Priodyuti Pradhan <priodyuti@users.noreply.github.com>
2022-08-14 18:34:41 +0530

    Delete Omega.d

--- Omega.d
+++
@@ -1,4 +0,0 @@
-/home/gautam/Dropbox/DCTSystem/obj/Omega.o: \
- /home/gautam/Dropbox/DCTSystem/src/Omega.cpp \
- /home/gautam/Dropbox/DCTSystem/src/Omega.h \
- /home/gautam/Dropbox/DCTSystem/src/Helper.h
